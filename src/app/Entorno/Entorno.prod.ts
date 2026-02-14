684560bde9539b54cf43ca1ce206f7d613c839ed
VictorSamines <saminesvictor@gmail.com>
2026-02-14 15:08:08 -0600

    feat: Add environment configuration files (`Entorno.ts` and `Entorno.prod.ts`) to define API URL, company details, and system color for different environments.

--- src/app/Entorno/Entorno.prod.ts
+++ src/app/Entorno/Entorno.prod.ts
@@ -1,6 +1,6 @@
 export const Entorno = {
   Produccion: true,
-  ApiUrl: 'https://puntoventa-promesadedios-api-production.up.railway.app/api/',
+  ApiUrl: 'https://puntoventa-promesadedios-api-production-29a8.up.railway.app/api/',
   NombreEmpresa: 'PromesaDeDios',
   Logo: 'logoPromesaDeDios.png',
   ColorSistema: '#FF9500'
--- src/app/Entorno/Entorno.ts
+++ src/app/Entorno/Entorno.ts
@@ -1,6 +1,6 @@
 export const Entorno = {
   Produccion: false,
-  ApiUrl: 'https://puntoventa-promesadedios-api-production.up.railway.app/api/',
+  ApiUrl: 'https://puntoventa-promesadedios-api-production-29a8.up.railway.app/api/',
   NombreEmpresa: 'PromesaDeDios',
   Logo: 'logoPromesaDeDios.png',
   // ColorSistema: '#FF9500'
